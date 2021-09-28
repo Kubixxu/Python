3337013dc6d77508b081d973cd3ab9889813e03b
Kubixxu <70577910+Kubixxu@users.noreply.github.com>
2021-09-28 17:17:43 +0200

    New addition

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ if len(sys.argv) == 1:
     print("Hello world!")
 
 for name_index in range(1, len(sys.argv)):
-    print(f"Hello {sys.argv[name_index]}!")
+    print(f"Hello {sys.argv[name_index]}! I committed")
